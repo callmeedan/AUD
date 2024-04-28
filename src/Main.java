f029bbf172c6af0a43d0c19fad13e052f0b88a0c
_callmedan_ <ducanhnguyen11062001@gmail.com>
2024-04-28 13:15:26 +0200

    add help method randomArrayGenerator

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,29 @@
 import java.util.Arrays;
+import java.util.Random;
 
+import SortingAlgorithms.*;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) throws Exception {
-        int[] test = {123, 245, 23, 26, 134, 634, 118, 611, 148};
-        Sort.radixSort(test, 3, 10);
-        System.out.println(Arrays.toString(test));
+        int[] a = randomArrayGenerator(10);
+
+    }
+
+    private static int[] randomArrayGenerator(int size){
+        int[] a = new int[size];
+        Random random = new Random();
+        for (int i = 0; i < a.length; i++){
+            a[i] = random.nextInt(0, 1000);
+            if (i>=1) {
+                for (int j = 0; j < i; j++) {
+                    while (a[j] == a[i]) {
+                        a[i] = random.nextInt(0, 1000);
+                    }
+                }
+            }
+        }
+        System.out.println(Arrays.toString(a));
+        return a;
     }
 }
